5dabf2bcc4d12adb96d5858324209dcb43550d88
Pamela Kling <pamela.k.kling@gmail.com>
2016-04-23 13:13:30 -0700

    JS file

---
+++ RockPaperScissors.js
@@ -0,0 +1,28 @@
+var userChoice;
+var correctAnswer = "A";
+
+function compare (cChoice, correctAnswer) {
+  if (uChoice === correctAnswer)
+  return (whoWon = "Correct!");
+  else (uChoice !== correctAnswer)
+  return "Wrong!";
+}
+
+prompt("This is the question. Type A or B.")
+
+function(){
+  if u
+}
+
+alert("This is the question. Type A or B.");
+
+do
+ userChoice = prompt("A-Correct, B-Incorrect");
+ if (userChoice === "A" || userChoice === "B") {
+ 	computerGuess();
+ 	alert(compare(correctAnswer, userChoice) + "\n" + "Computer Choice: " + computerChoice + "\t" + "Your Choice: " + userChoice);
+
+ 	if (confirm("Press OK to play AGAIN!") == false)
+ 	validChoice = true;
+ }
+} while(!validChoice);
